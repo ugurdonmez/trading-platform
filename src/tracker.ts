4158e19b94f78e86afb65f48335cad50bb269cdd
Donmez, Ugur <ugurdonmez87@gmail.com>
2023-08-28 12:51:51 +0200

    get webhook url from env

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,8 +1,11 @@
 import { RSITracker } from "./process/RSITracker";
 import { INotifier } from "./notification/INotifier";
-import {DiscordNotifier} from "./notification/discord/DiscordNotifier";
+import { DiscordNotifier } from "./notification/discord/DiscordNotifier";
 
-const discordWebhookUrl = 'YOUR_DISCORD_WEBHOOK_URL_HERE';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL!;
 const notifier = new DiscordNotifier(discordWebhookUrl);
 const rsiTracker = new RSITracker("GPTUSDT", notifier, "1h");
 
