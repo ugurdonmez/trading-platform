7a34e48b28441123713e1fb0f5aade216c5c129c
Donmez, Ugur <ugurdonmez87@gmail.com>
2023-09-07 19:50:45 +0200

    cron

---
+++ src/cron/RSITrackerCron.ts
@@ -0,0 +1,40 @@
+import { RSITracker } from "../process/RSITracker";
+import { DiscordNotifier } from "../notification/discord/DiscordNotifier";
+import { ExchangeFactory } from "../exchanges/ExchangeFactory";
+import { ExchangeConfig } from "../models/ExchangeConfig";
+import { AppConfig } from "../models/AppConfig";
+
+import * as rawConfig from '../../config.json';
+
+import dotenv from 'dotenv';
+import logger from "../logger/Logger";
+dotenv.config();
+
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK_URL!;
+const notifier = new DiscordNotifier(discordWebhookUrl);
+
+const API_KEY = process.env.BYBIT_API_KEY!;
+const SECRET_KEY = process.env.BYBIT_SECRET_KEY!;
+
+const bybitConfig: ExchangeConfig = {
+    name: 'bybit',
+    apiKey: API_KEY,
+    secret: SECRET_KEY,
+};
+
+const config: AppConfig = rawConfig as AppConfig;
+
+const bybitExchange = ExchangeFactory.create(bybitConfig);
+
+// TODO: get exchange also from config
+const rsiTrackers = config.rsiTrackers.map(trackerConfig => new RSITracker(trackerConfig.symbol, notifier, trackerConfig.timeframe, bybitExchange));
+
+async function checkAllRSIs() {
+    for (const rsiTracker of rsiTrackers) {
+        logger.info(`Checking RSI process started for pair: ${rsiTracker.getPair()}`);
+        await rsiTracker.checkRSI();
+    }
+}
+
+// Calling the function directly.
+checkAllRSIs();
