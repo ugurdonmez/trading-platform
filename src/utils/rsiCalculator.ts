66edf5a79a21d1114d18cc07843e547d58664a1d
Donmez, Ugur <ugurdonmez87@gmail.com>
2023-09-08 10:43:10 +0200

    fix rsi calculator

--- src/utils/rsiCalculator.ts
+++ src/utils/rsiCalculator.ts
@@ -1,6 +1,14 @@
 import * as talib from 'ta-lib';
 
 export const calculate_rsi = (closePrices: number[], rsiLength: number) => {
-    const rsi = talib.RSI(closePrices, rsiLength);
+
+    //console.log(`Calculating RSI for ${closePrices.length} prices with length ${rsiLength}`);
+    //console.log(closePrices.join(', '));
+
+
+    const rsi = talib.RSI(closePrices.reverse(), rsiLength);
+
+    //console.log(`RSI calculated: ${rsi}`);
+
     return rsi[rsi.length - 1];
 }
