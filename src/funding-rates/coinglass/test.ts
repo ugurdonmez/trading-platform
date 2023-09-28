0a7e64750080871692045bebab6a4c30135f87cb
Donmez, Ugur <ugurdonmez87@gmail.com>
2023-09-28 23:08:22 +0300

    funding rate test

--- src/funding-rates/coinglass/test.ts
+++ src/funding-rates/coinglass/test.ts
@@ -15,9 +15,29 @@ async function getFundingRates() {
         const response = await axios(options);
         const fundingRates: FundingRate[] = response.data.data;
 
-        const btcFundingRate = fundingRates.find(f => f.symbol === 'BTC');
+        let btcFundingRate = fundingRates.find(f => f.symbol === 'TRB');
 
-        console.log(JSON.stringify(btcFundingRate));
+        // do it for all symbols
+        fundingRates.forEach(f => {
+            f.uMarginList = f.uMarginList.filter(m => m.status === 1 || m.status === 2);
+            f.uMarginList = f.uMarginList.filter(m => m.exchangeName === 'Binance' || m.exchangeName === 'Bybit' || m.exchangeName === 'OKX');
+            f.cMarginList = [];
+            f.symbolLogo = '';
+        });
+
+        // if (btcFundingRate) {
+        //     // filter out the exchanges that are not active
+        //     btcFundingRate.uMarginList = btcFundingRate.uMarginList.filter(m => m.status === 1 || m.status === 2);
+
+        //     // filter only get binance bybit and okx
+        //     btcFundingRate.uMarginList = btcFundingRate.uMarginList.filter(m => m.exchangeName === 'Binance' || m.exchangeName === 'Bybit' || m.exchangeName === 'OKX');
+
+        //     console.log(JSON.stringify(btcFundingRate.uMarginList));
+        // } else {
+        //     console.log('BTC funding rate not found');
+        // }
+
+        console.log(JSON.stringify(fundingRates));
 
         // console.log(JSON.stringify (fundingRates));
     } catch (error) {
